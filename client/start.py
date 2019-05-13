4373461606b712af20d7c4c00a498d053b28646d
Jacob Reckhard <jacobreckhard@gmail.com>
2019-05-13 22:56:31 +0000

    added start script

---
+++ client/start.py
@@ -0,0 +1,14 @@
+import tornado.ioloop
+import tornado.web
+import tornado.autoreload
+import os
+
+application = tornado.web.Application([
+    (r"/()$", tornado.web.StaticFileHandler, {'path':'./index.html'}),
+    (r"/(.*)", tornado.web.StaticFileHandler, {"path": "."}),
+],debug=True)
+
+if __name__ == "__main__":
+    tornado.autoreload.watch(os.path.abspath('.'))
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
