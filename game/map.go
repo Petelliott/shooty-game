51e311752fdf61718149e38ee9b62a41978b41e0
Jacob Reckhard <jacobreckhard@gmail.com>
2019-05-15 22:57:31 +0000

    made map a real thing

--- game/map.go
+++ game/map.go
@@ -1,4 +1,19 @@
 package game
 
-//TODO: make a map type
-type Map int
+type Map struct {
+	Tiles          []byte          `json:"tiles"`
+	size           int             `json:"size"`
+	TileDescriptor map[byte]string `json:"tileDescription"`
+}
+
+func NewMap(size int) Map {
+	tiles := make([]byte, size*size)
+	td := map[byte]string{
+		0: "ground",
+	}
+	return Map{tiles, size, td}
+}
+
+func (m *Map) MarshalJSON() ([]byte, error) {
+	return json.Marshal(m)
+}
--- sgs/main.go
+++ sgs/main.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
-    "github.com/gin-contrib/cors"
-    "os"
-    "github.com/petelliott/shooty-game/game"
-    "github.com/petelliott/shooty-game/dynconfig"
-    "github.com/petelliott/shooty-game/jsonconfig"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/petelliott/shooty-game/dynconfig"
+	"github.com/petelliott/shooty-game/game"
+	"github.com/petelliott/shooty-game/jsonconfig"
+	"os"
 )
 
 func main() {
-    dc := dynconfig.Open(os.Args[1])
-    jc := jsonconfig.Open(os.Args[2])
+	dc := dynconfig.Open(os.Args[1])
+	jc := jsonconfig.Open(os.Args[2])
 
-    game.NewStandardGame(&dc, &jc, 59)
+	game.NewStandardGame(&dc, &jc, game.NewMap(10))
 
-    router := gin.Default()
+	router := gin.Default()
 
-    config := cors.DefaultConfig()
+	config := cors.DefaultConfig()
 	config.AllowAllOrigins = true
-    router.Use(cors.New(config))
+	router.Use(cors.New(config))
 
-    router.Run(":8049")
+	router.Run(":8049")
 }
