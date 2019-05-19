25d9dc0c88ee4fd19cd880b200e3d7371f198033
Peter Elliott <pelliott@ualberta.ca>
2019-05-18 19:26:50 -0700

    fixed invalid json in config dir

--- jsondir/d2json/main.go
+++ jsondir/d2json/main.go
@@ -4,10 +4,14 @@ import (
 	"github.com/petelliott/shooty-game/jsondir"
 	"encoding/json"
 	"os"
+	"runtime/debug"
 )
 
 func main() {
-	b, _ := json.MarshalIndent(jsondir.Jdir(os.Args[1]), "", "    ")
+	b, err := json.MarshalIndent(jsondir.Jdir(os.Args[1]), "", "    ")
+	if err != nil {
+		panic(err)
+	}
 	os.Stdout.Write(b)
 	os.Stdout.Write([]byte{'\n'})
 }
